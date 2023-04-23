52651c82506cda311abb9463ad478cccfcd09b5b
GeumBin <lgb9811@gmail.com>
2023-04-23 22:22:39 +0900

    ALLWED_HOST 및 STATIC_ROOT 추가

--- slackbot/settings.py
+++ slackbot/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-4*+@gi6jf2w*znw&e0r&pi@r%qr89@*5n9^r0k17bd=8=2!7j&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -106,6 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
